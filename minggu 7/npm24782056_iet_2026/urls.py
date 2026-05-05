65e4f547752f9ea51b429ed54720519f33078372
naila24782056 <nailasalsabilla0627@gmail.com>
2026-05-05 21:11:30 +0700

    lab 7

---
+++ minggu 7/npm24782056_iet_2026/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main_app.urls')),
+    path('dashboard/', include('dashboard_24782056.urls')),
+    path('about/', include('about.urls')),
+    path('contacts/', include('contacts.urls')),
+    path('auth/', include('usermanagement_24782056.urls')),
+]
